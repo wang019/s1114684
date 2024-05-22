2701b3b66b40e9560c5708baa9875f89b19fa470
wang019 <Rita749749@gmail.com>
2024-05-22 17:45:56 +0800

    v2_王怡蓁

--- app/src/main/java/edu/pu/tw/wang/s1114684/MainActivity.kt
+++ app/src/main/java/edu/pu/tw/wang/s1114684/MainActivity.kt
@@ -3,13 +3,33 @@ package edu.pu.tw.wang.s1114684
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import edu.pu.tw.wang.s1114684.ui.theme.S1114684Theme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +38,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             S1114684Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("簡介")
                 }
             }
         }
@@ -29,8 +52,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-            text = "Hello $name!",
-            modifier = modifier
+        text = name,
+        modifier = modifier,
+        color = Color.Blue
     )
 }
 
@@ -38,6 +62,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     S1114684Theme {
-        Greeting("Android")
+        Greeting("簡介")
     }
 }
